a61944e8c72d7287f457a7c3787c12c5ab1aa2b3
Abubakar Jilani <abub1156@gmail.com>
2023-12-07 13:51:26 +0500

    created delete api

--- server/index.js
+++ server/index.js
@@ -44,13 +44,9 @@ app.get("/products", async (req, res) => {
   }
 });
 
-app.delete("/delete-products", async (req, res) => {
-  let productList = await ProductModel.find();
-  if (productList.length > 0) {
-    res.send(productList);
-  } else {
-    res.send({ result: "No product found" });
-  }
+app.delete("/product/:id", async (req, res) => {
+  const result = await ProductModel.deleteOne({ _id: req.params.id });
+  res.send({ result });
 });
 
 app.listen(5000);
